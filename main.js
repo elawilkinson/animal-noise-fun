69d76660c7d45cbed3467be3dde032583675cd18
elawilkinson <mailtoelaw454@gmail.com>
2022-05-05 19:43:35 +0100

    File setup and plan outlined

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("java here!");
+
+// ## PLAN
+// # Find (5) animals 
+// # Store their noises in English
+// # Store their noises in French
+// # Store a list of excellent noises as a bonus option
+// # Find pictures of the animals
+// # Create randomise option for animals
+// # Create randomise option for language
+// # Create animal button
+// # Create language button
+// # Create display window for final result
+// # Display chosen animal in h2
+// # Display chosen language in h2
